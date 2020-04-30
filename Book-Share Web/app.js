c183b647029eae8feadcd92a77ccf6f7144ed6da
Shaunak <gamershaunj@gmail.com>
2020-04-30 11:33:27 +0530

    updated files

--- Book-Share Web/app.js
+++ Book-Share Web/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+var JSAlert = require("js-alert");
 
 const app = express();
 
@@ -9,6 +10,15 @@ app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true , useUnifiedTopology: true })
+
+const userSchema = new mongoose.Schema({
+  username : String,
+  password: String
+})
+
+const User = mongoose.model('User',userSchema);
 
 
 app.get("/",function(req,res){
@@ -21,18 +31,35 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
   const userName = req.body.username;
-  const password = req.body.password;
-  console.log(userName);
-  console.log(password);
+  const passWord = req.body.password;
+  
+ 
+  User.find({username: userName},function(err,user){
+    if (!err){
+     
+      if (user){
+        console.log(user)
+        //res.render("register",{success: "User already exists, please create another"})
+      }
+      else{
+        const user = new User({username: userName, password: passWord})
+        user.save(function(err){
+          if (!err){
+            console.log("User inserted");
+          }
+        })
+      }
+    }
+    
+    
+  })
+
 })
 
 
 app.get("/login",function(req,res){
   res.render("login");
 })
-
-
-
 
 
 
